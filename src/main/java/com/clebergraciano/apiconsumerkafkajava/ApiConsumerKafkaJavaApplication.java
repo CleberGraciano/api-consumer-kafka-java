beb9fdee847f42d5de0a6cf088e4101dbe172ab6
Cleber <cleber_batista2010@hotmail.com>
2022-06-18 17:56:37 -0300

    Criando aplicação consumer Kafka

---
+++ src/main/java/com/clebergraciano/apiconsumerkafkajava/ApiConsumerKafkaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.clebergraciano.apiconsumerkafkajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsumerKafkaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsumerKafkaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clebergraciano/apiconsumerkafkajava/ApiConsumerKafkaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clebergraciano.apiconsumerkafkajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsumerKafkaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
